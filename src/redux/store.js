399f7502571bc50eb42138c603c5dd4ec3532460
Hsiang <q224399377@gmail.com>
2022-04-27 00:06:17 +0800

    edit store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,8 @@ import counterReducer from './counterSlice'
 const store = configureStore({
     reducer: {
       counter: counterReducer,
-    }
+    },
+    devTools: process.env.NODE_ENV !== 'production'
   });
 
   export default store;
